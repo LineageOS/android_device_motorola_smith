2490daa65cf8e5b0f148f4afdf833186b3614079
Andrew Hexen <SyberHexen@gmail.com>
2022-02-02 06:25:39 -0800

    smith: add vendor interface for android.hardware.biometrics.fingerprint
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I59b0e366dbb5ed59e1ac42f0604f7939da88f9b0

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor
+
 # Fold related configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fold/device_state_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/devicestate/device_state_configuration.xml \
