c0b14b25d67f99f51d84c91922e1d78c72ba2af7
dianlujitao <dianlujitao@lineageos.org>
2022-06-07 19:41:04 +0800

    smith: Switch to stock volume curves
    Change-Id: I550b524741efb24577121cdb8c779feca8dcb032

--- device.mk
+++ device.mk
@@ -41,7 +41,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_madera_princeb0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_madera_princeb0.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
 # Init
 PRODUCT_PACKAGES += \
