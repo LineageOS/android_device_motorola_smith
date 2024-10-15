2f7a496bf8420bf64fd64b6032024bfa8dce792a
LuK1337 <priv.luk@gmail.com>
2024-10-15 22:11:44 +0200

    smith: Switch to py extract utils
    Change-Id: I5618bf10c107df579a540e2393b222128961e670

---
+++ extract-files.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+namespace_imports = [
+    'vendor/motorola/sm7250-common',
+    'vendor/qcom/opensource/display',
+]
+blob_fixups: blob_fixups_user_type = {
+    # Patch configureRpcThreadpool 
+    'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
+        .sig_replace('CC 0A 00 94', '1F 20 03 D5'),
+    # memset shim
+    'vendor/bin/charge_only_mode': blob_fixup()
+        .add_needed('libmemset_shim.so'),
+    # rename moto modified tinyalsa
+    ('vendor/lib/motorola.hardware.audio.adspd@1.0-impl.so', 'vendor/lib64/motorola.hardware.audio.adspd@1.0-impl.so'): blob_fixup()
+        .replace_needed('libtinyalsa.so', 'libtinyalsa-moto.so'),
+    # __lttf2 shim 
+    'vendor/lib64/libvidhance.so': blob_fixup()
+        .add_needed('libcomparetf2_shim.so'),
+}  # fmt: skip
+module = ExtractUtilsModule(
+    'smith',
+    'motorola',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    add_firmware_proprietary_file=False,
+)
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm7250-common', module.vendor
+    )
+    utils.run()
